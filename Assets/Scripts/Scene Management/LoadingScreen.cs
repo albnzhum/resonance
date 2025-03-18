969c8721e41df26a8a6d8cb6fe4b8a3aeecbc835
grandfrog0 <maksspecial32@gmail.com>
2025-03-18 16:52:06 +0500

    Load scene, scene loading screen added

---
+++ Assets/Scripts/Scene Management/LoadingScreen.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.UI;
+
+/// <summary>
+/// Анимированный экран загрузки
+/// </summary>
+public class LoadingScreen : MonoBehaviour
+{
+    public static LoadingScreen instance;
+
+    [SerializeField] Image loadScreenBackground;    // фон экрана загрузки
+    [SerializeField] RectTransform loadScreenImage; // нестатичное изображение во время загрузки
+    [SerializeField] float fadeSpeed = 0.1f;
+    [SerializeField] float rotateSpeed = 10f;
+
+    public void LoadAnimation(YieldInstruction waitFor)
+    {
+        StopAllCoroutines();
+        StartCoroutine(LoadAnimationEnum(waitFor));
+    }
+
+    private IEnumerator LoadAnimationEnum(YieldInstruction waitFor)
+    {
+        Coroutine rotateSquare = StartCoroutine(RotateSquare());
+        Coroutine moveSquare = StartCoroutine(MoveSquare(Vector2.zero));
+
+        loadScreenBackground.color = new Color(loadScreenBackground.color.r, loadScreenBackground.color.g, loadScreenBackground.color.b, 1);
+
+        yield return waitFor;
+        yield return new WaitForSeconds(1);
+
+        if (rotateSquare != null) StopCoroutine(rotateSquare);
+        if (moveSquare != null) StopCoroutine(moveSquare);
+
+        moveSquare = StartCoroutine(MoveSquare(Vector2.up * Screen.height));
+
+        for (float i = 1; i >= 0; i -= fadeSpeed / 2)
+        {
+            loadScreenBackground.color = new Color(loadScreenBackground.color.r, loadScreenBackground.color.g, loadScreenBackground.color.b, i);
+            yield return null;
+        }
+
+        yield return moveSquare;
+    }
+
+    private IEnumerator RotateSquare()
+    {
+        while (true)
+        {
+            loadScreenImage.Rotate(0, 0, rotateSpeed);
+            yield return null;
+        }
+    }
+
+    private IEnumerator MoveSquare(Vector2 to)
+    {
+        float start = Time.time;
+
+        while (Vector3.Distance(loadScreenImage.anchoredPosition, to) > 1)
+        {
+            loadScreenImage.anchoredPosition = Vector2.Lerp(loadScreenImage.anchoredPosition, to, (Time.time  - start) * fadeSpeed * 2);
+
+            yield return null;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        instance = this;
+    }
+}
