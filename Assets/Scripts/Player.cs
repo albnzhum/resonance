4b018bb1199a34b7b8316147ef663b19ac5d5ae2
albnzhum <albnzhum@gmail.com>
2025-03-20 11:31:15 +0500

    Edited player controller

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private CharacterController cc;
     PlayerController _controller;
+    
+    public ParticleSystem ripple;
+    private RaycastHit isGround;
+
+    private bool inWater;
 
     public bool IsRunning { get { return _controller.IsRunning; } }
 
     private void Start()
     {
         _controller = GetComponent<PlayerController>();
+    }
+
+    private void Update()
+    {
+        if (inWater) ripple.gameObject.SetActive(true);
+        else ripple.gameObject.SetActive(false);
+        
+        Physics.Raycast(transform.position, Vector3.down, out isGround, 2.7f, LayerMask.GetMask("Ground"));
+        float height = cc.height + cc.radius;
+        inWater = Physics.Raycast(transform.position + Vector3.up * height, Vector3.down, height * 2,
+            LayerMask.GetMask("Water"));
+        
+        if (isGround.collider) ripple.transform.position = transform.position + transform.forward;
+        else ripple.transform.position = transform.position;
+        
+        Shader.SetGlobalVector("_Player", transform.position);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -27,5 +50,20 @@ public class Player : MonoBehaviour
         collision.TryGetComponent(out interactable);
         if (interactable != null)
             interactable.Interact(this);
+        
+        if (collision.gameObject.layer == 4)
+        {
+            //CreateRipple(-180, 180, 2, 5, 3f, 3);
+            ripple.Emit(transform.position, Vector3.zero, 5, 0.1f, Color.white);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 4)
+        {
+            //CreateRipple(-180, 180, 2, 5, 3f, 3);
+            ripple.Emit(transform.position, Vector3.zero, 5, 0.1f, Color.white);
+        }
     }
 }
