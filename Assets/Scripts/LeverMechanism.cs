1b1206340fb3e255fa0abdca26c52585e241f496
Артем Клейменов <lenovaega2@gmail.com>
2025-03-23 20:42:11 +0500

    работа над нажатием птицей рычага

--- Assets/Scripts/LeverMechanism.cs
+++ Assets/Scripts/LeverMechanism.cs
@@ -1,59 +1,35 @@
 using UnityEngine;
 using System.Collections;
+using TMPro;
 
 public class LeverMechanism : MonoBehaviour
 {
-    //public Transform pivot; // Точка вращения/опускания рычага
-    //public float moveDistance = 0.2f; // Насколько опускается рычаг
-    //public float moveSpeed = 2f; // Скорость опускания
-    //public GameObject linkedObject; // Объект, который активируется (например, дверь)
-
-    //private bool isActivated = false;
-    //private Transform activatorUnit; // Ворона, сидящая на рычаге
-
-    //public void ActivateLever(Transform activatorTransform)
-    //{
-    //    if (isActivated) return;
-
-    //    isActivated = true;
-    //    activatorUnit = activatorTransform;
-    //    StartCoroutine(LowerLever());
-    //}
-
-    //private IEnumerator LowerLever()
-    //{
-    //    float currentAngle = 0f;
-    //    float targetAngle = 45f; // или -45f, зависит от направления
-    //    float speed = 90f; // градусов в секунду
-
-    //    while (currentAngle < targetAngle)
-    //    {
-    //        float step = speed * Time.deltaTime;
-    //        leverArm.Rotate(step, 0, 0); // Вращение по локальной оси X (если палка стоит вертикально)
-    //        currentAngle += step;
-    //        yield return null;
-    //    }
-    //}
-
     [SerializeField] Transform pivot;
+    [SerializeField] Transform leverTopPoint;
     [SerializeField] float currentAngle = 0f;
-    [SerializeField] float targetAngle = 45f; // или -45f, зависит от направления
-    [SerializeField] float speed = 90f; // градусов в секунду
+    [SerializeField] float targetAngle = 45f;
+    [SerializeField] float speed = 90f;
 
-    public void LowerLever()
+    public void LowerLeverByBird(Bird bird)
     {
-        StartCoroutine(LowerLeverCoroutine());
+        StartCoroutine(LowerLeverByBirdCoroutine(bird));
     }
 
-    private IEnumerator LowerLeverCoroutine()
+    private IEnumerator LowerLeverByBirdCoroutine(Bird bird)
     {
         while (currentAngle < targetAngle)
         {
             float step = speed * Time.deltaTime;
-            pivot.Rotate(step, 0, 0); // Вращение по локальной оси X (если палка стоит вертикально)
+            pivot.Rotate(0, 0, step);
             currentAngle += step;
+
+            Vector3 offset = transform.position - bird.GetPawsPosition();
+
+            Vector3 adjustedTarget = leverTopPoint.position + offset;
+
+            bird.transform.position = adjustedTarget;
+
             yield return null;
         }
-        // Вызов логики активации: открыть дверь, включить механизм и т.п.
     }
 }
